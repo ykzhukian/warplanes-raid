796b752a6a2cc8b26b7799f7d0f05aed78d3eff0
Kian Zhu <kianzyk@gmail.com>
2021-01-13 23:19:47 +0800

    f

--- src/views/Game.ts
+++ src/views/Game.ts
@@ -2,6 +2,7 @@ import { defineComponent, h } from '@vue/runtime-core';
 import map from '../assets/images/map.svg';
 
 export default defineComponent({
+
   render() {
     return h('Container', [
       h('Sprite', { texture: map }),
